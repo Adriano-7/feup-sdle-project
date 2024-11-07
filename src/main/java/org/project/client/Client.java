99cb0b3202d2ffae12a662201bf75a4090aa634b
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2024-11-07 10:17:16 +0000

    Configured Gradle

---
+++ src/main/java/org/project/client/Client.java
@@ -0,0 +1,7 @@
+package org.project.client;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World! from client");
+    }
+}
---
+++ src/main/java/org/project/server/Server.java
@@ -0,0 +1,7 @@
+package org.project.server;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello World! from server");
+    }
+}
