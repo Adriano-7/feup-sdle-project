814fcd1e7c3aee307fbe1b2980a6cf5b81c7d2f3
Inês Alexandre Queirós Matos Macedo de Oliveira <up202103343@up.pt>
2024-11-28 08:51:53 +0000

    load balancer implementation

---
+++ src/main/java/org/project/server/LoadBalancer.java
@@ -0,0 +1,46 @@
+package org.project.server;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class LoadBalancer {
+    private final List<String> servers;
+    private final SortedMap<Integer, String> hashRing;
+
+    public LoadBalancer() {
+        servers = new ArrayList<>();
+        hashRing = new TreeMap<>();
+    }
+
+    public void addServer(String serverAddress) {
+        int hash = serverAddress.hashCode();
+        hashRing.put(hash, serverAddress);
+        servers.add(serverAddress);
+        System.out.println("Servidor adicionado: " + serverAddress);
+    }
+
+    public void removeServer(String serverAddress) {
+        int hash = serverAddress.hashCode();
+        hashRing.remove(hash);
+        servers.remove(serverAddress);
+        System.out.println("Servidor removido: " + serverAddress);
+    }
+
+    public String getServer(String key) {
+        if (hashRing.isEmpty()) {
+            throw new IllegalStateException("Nenhum servidor disponível no Hash Ring!");
+        }
+
+        int hash = key.hashCode();
+        // Search the next bigger hash in hash ring
+        SortedMap<Integer, String> tailMap = hashRing.tailMap(hash);
+        Integer serverHash = tailMap.isEmpty() ? hashRing.firstKey() : tailMap.firstKey();
+        return hashRing.get(serverHash);
+    }
+
+    public List<String> getServers() {
+        return servers;
+    }
+}
--- src/main/java/org/project/server/Server.java
+++ src/main/java/org/project/server/Server.java
@@ -2,6 +2,8 @@ package org.project.server;
 
 public class Server {
     public static void main(String[] args) {
+        LoadBalancer loadBalancer = new LoadBalancer();
+
         System.out.println("Hello World! from server");
     }
 }
