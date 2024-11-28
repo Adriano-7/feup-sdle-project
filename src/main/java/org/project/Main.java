23982721acc46363b95181b9529eecc32c66c358
Carolina Brandão <up202004461@edu.fc.up.pt>
2024-11-28 10:02:51 +0000

    merge with master

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,7 +1,35 @@
 package org.project;
 
+import org.project.client.Client;
+import org.project.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! from main");
+        if (args.length == 0) {
+            System.out.println("Please specify 'server' or 'client' as an argument.");
+            return;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "server" -> {
+                System.out.println("Starting server...");
+                try {
+                    Server.main(new String[]{});
+                } catch (Exception e) {
+                    System.err.println("Error while running the server: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            case "client" -> {
+                System.out.println("Starting client...");
+                Client.main(new String[]{}); // Call the client's main method
+            }
+
+            default -> {
+                System.out.println("Unknown argument: " + args[0]);
+                System.out.println("Use 'server' to run the server or 'client' to run the client.");
+            }
+        }
     }
 }
