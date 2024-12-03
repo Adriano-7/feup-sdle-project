90e5b71e1b31c118bfdb9a57bcf7fa8598c5bbea
Carolina Brandão <up202004461@edu.fc.up.pt>
2024-12-03 14:47:13 +0000

    merge?

--- src/main/java/org/project/client/Client.java
+++ src/main/java/org/project/client/Client.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 import java.util.UUID;
 
 import org.project.model.ShoppingList;
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 
 public class Client {
@@ -98,40 +101,26 @@ public class Client {
 
     }
 
-    /*public static void xmq(String[] args)
+    public static void xmq(String[] args)
     {
         try (ZContext context = new ZContext()) {
             //  Socket to talk to server
-            System.out.println("Collecting updates from weather server");
-            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
-            subscriber.connect("tcp://localhost:5556");
+            System.out.println("Connecting to hello world server");
 
-            //  Subscribe to zipcode, default is NYC, 10001
-            String filter = (args.length > 0) ? args[0] : "10001 ";
-            subscriber.subscribe(filter.getBytes(ZMQ.CHARSET));
+            ZMQ.Socket socket = context.createSocket(SocketType.REQ);
+            socket.connect("tcp://localhost:5555");
 
-            //  Process 100 updates
-            int update_nbr;
-            long total_temp = 0;
-            for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-                //  Use trim to remove the tailing '0' character
-                String string = subscriber.recvStr(0).trim();
+            for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
+                String request = "Hello";
+                System.out.println("Sending Hello " + requestNbr);
+                socket.send(request.getBytes(ZMQ.CHARSET), 0);
 
-                StringTokenizer sscanf = new StringTokenizer(string, " ");
-                int zipcode = Integer.valueOf(sscanf.nextToken());
-                int temperature = Integer.valueOf(sscanf.nextToken());
-                int relhumidity = Integer.valueOf(sscanf.nextToken());
-
-                total_temp += temperature;
+                byte[] reply = socket.recv(0);
+                System.out.println(
+                    "Received " + new String(reply, ZMQ.CHARSET) + " " +
+                    requestNbr
+                );
             }
-
-            System.out.println(
-                String.format(
-                    "Average temperature for zipcode '%s' was %d.",
-                    filter,
-                    (int)(total_temp / update_nbr)
-                )
-            );
         }
-    }*/
+    }
 }
--- src/main/java/org/project/server/Server.java
+++ src/main/java/org/project/server/Server.java
@@ -1,7 +1,5 @@
 package org.project.server;
 
-import java.util.Random;
-
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
@@ -10,26 +8,21 @@ public class Server
 {
     public static void main(String[] args) throws Exception
     {
-        //  Prepare our context and publisher
         try (ZContext context = new ZContext()) {
-            ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
-            publisher.bind("tcp://*:5556");
-            publisher.bind("ipc://weather");
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
+            socket.bind("tcp://*:5555");
 
-            //  Initialize random number generator
-            Random srandom = new Random(System.currentTimeMillis());
             while (!Thread.currentThread().isInterrupted()) {
-                //  Get values that will fool the boss
-                int zipcode, temperature, relhumidity;
-                zipcode = 10000 + srandom.nextInt(10000);
-                temperature = srandom.nextInt(215) - 80 + 1;
-                relhumidity = srandom.nextInt(50) + 10 + 1;
-
-                //  Send message to all subscribers
-                String update = String.format(
-                    "%05d %d %d", zipcode, temperature, relhumidity
+                byte[] reply = socket.recv(0);
+                System.out.println(
+                    "Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]"
                 );
-                publisher.send(update, 0);
+
+                Thread.sleep(1000); //  Do some 'work'
+
+                String response = "world";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
             }
         }
     }
